d37494553dd804d01af3b5589113d56e8047f884
NghiaNH <mickwan1234@gmail.com>
2020-01-15 13:29:48 +0700

    first commit

--- src/app/App.java
+++ src/app/App.java
@@ -1,7 +1,10 @@
 package app;
 
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java");
+        ArrayList arrayList = new ArrayList();
     }
 }
