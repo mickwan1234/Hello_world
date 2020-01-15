8bbfd50fa8ffbcd5be26a88c5ca77ab98b7b7f05
NghiaNH <mickwan1234@gmail.com>
2020-01-15 13:34:02 +0700

    Second commit

--- src/app/App.java
+++ src/app/App.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java");
-        ArrayList arrayList = new ArrayList();
+        ArrayList<String> arrayList = new ArrayList<String>();
+        arrayList.add("Hello");
+        arrayList.add("World");
+        System.out.println(arrayList.toString());
     }
 }
